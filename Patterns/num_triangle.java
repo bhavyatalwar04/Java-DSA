73a7f72dcb1bff1d3cad8167590dabc96691ada1
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2025-12-30 11:53:47 +0530

    pattern

--- Patterns/num_triangle.java
+++ Patterns/num_triangle.java
@@ -4,7 +4,7 @@ class num_triangle {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(i + "");
+                System.out.print(j + "");
             }
             System.out.println("");
         }
