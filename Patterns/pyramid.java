85bf6470124245a19bf0a09b292f63c8e4f1798a
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2025-12-31 11:19:02 +0530

    changes

--- Patterns/pyramid.java
+++ Patterns/pyramid.java
@@ -1,13 +1,14 @@
 
-class pattern5 {
+class pyramid {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5 - i + 1; j++) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println("");
+
         }
     }
+
 }
