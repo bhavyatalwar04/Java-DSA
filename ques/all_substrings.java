30f762b33188204452de5c321334522ce51aba64
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-01-24 15:29:38 +0530

    changes

---
+++ ques/all_substrings.java
@@ -0,0 +1,13 @@
+
+public class all_substrings {
+
+    public static void main(String[] args) {
+        String str = "abc";
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                System.out.println(str.substring(i, j));
+            }
+        }
+    }
+}
