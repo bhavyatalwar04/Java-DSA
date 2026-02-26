13a9d04f4b3cead403df8a466f86fdccf8fd8e74
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-02-26 14:41:55 +0530

    changes

--- Technical Class/fact.java
+++ Technical Class/fact.java
@@ -4,7 +4,6 @@ public class fact {
     public static void main(String[] args) {
         int n = 5;
         System.out.println(facto(n));
-        he();
     }
 
     public static int facto(int n) {
@@ -13,10 +12,5 @@ public class fact {
             return 1;
         }
         return n * facto(n - 1);
-    }
-
-    public static void he() {
-        System.out.println("hi");
-        return;
     }
 }
