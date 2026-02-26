b0c95b265c2ae4195d86b0386d48321882052b46
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-02-26 14:51:10 +0530

    changes

--- Technical Class/fact.java
+++ Technical Class/fact.java
@@ -8,7 +8,7 @@ public class fact {
 
     public static int facto(int n) {
 
-        if (n == 0 || n == 1) {
+        if (n == 0) {
             return 1;
         }
         return n * facto(n - 1);
