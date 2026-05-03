7693db7cb6f16e3e84da67d8d47da781b3b0341d
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-05-03 12:46:18 +0530

    practice

--- ques/all_substrings.java
+++ ques/all_substrings.java
@@ -1,12 +1,25 @@
 
+// public class all_substrings {
+//     public static void main(String[] args) {
+//         String str = "abc";
+//         for (int i = 0; i < str.length(); i++) {
+//             for (int j = i + 1; j <= str.length(); j++) {
+//                 System.out.println(str.substring(i, j));
+//             }
+//         }
+//     }
+// }
 public class all_substrings {
 
     public static void main(String[] args) {
-        String str = "abc";
+        String st = "abc";
 
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = i + 1; j <= str.length(); j++) {
-                System.out.println(str.substring(i, j));
+        for (int i = 0; i < st.length(); i++) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = i; j < st.length(); j++) {
+                sb.append(st.charAt(j));
+                System.out.println(sb.toString());
             }
         }
     }
