3d1f72eeb5acb21c6ffea2042f24cb3818e70b07
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-05-29 12:01:14 +0530

    queue

---
+++ Queues/removekele.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class removekele {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        System.out.println(q);
+
+        //here k=3
+        int k = 3;
+        int n = q.size();
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < k; i++) {
+            st.push(q.poll());
+        }
+        while (!st.isEmpty()) {
+            q.offer(st.pop());
+        }
+        for (int i = 0; i < n - k; i++) {
+            q.offer(q.poll());
+        }
+        System.out.println(q);
+    }
+}
