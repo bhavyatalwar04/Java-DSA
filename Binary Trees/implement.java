83c2f6437386f8ff902918cd1259b12d0b4be3fd
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-05-29 18:51:20 +0530

    Trees

---
+++ Binary Trees/implement.java
@@ -0,0 +1,57 @@
+
+public class implement {
+
+    public static void main(String[] args) {
+        Node a = new Node(3);
+        Node b = new Node(4);
+        Node c = new Node(2);
+        Node d = new Node(-1);
+        Node e = new Node(1);
+        Node f = new Node(6);
+        Node g = new Node(9);
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.left = f;
+        c.right = g;
+
+        // display(a);
+        System.out.println(size(a));
+        System.out.println(sum(a));
+
+    }
+
+    // private static void display(Node root) {
+    //     if (root == null) {
+    //         return;
+    //     }
+    //     System.out.println(root.val);
+    //     display(root.left);
+    //     display(root.right);
+    // }
+    private static int size(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + size(root.left) + size(root.right);
+    }
+
+    private static int sum(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return root.val + sum(root.left) + sum(root.right);
+    }
+
+    static class Node {
+
+        int val;
+        Node left;
+        Node right;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+}
---
+++ Binary Trees/tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+    // private static void display(Node root) {
+    //     if (root == null) {
+    //         return;
+    //     }
+    //     System.out.println(root.val);
+    //     display(root.left);
+    //     display(root.right);
+    // }
